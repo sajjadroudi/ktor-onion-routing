0181d7c0cc1c76b4abd4f18d58151d6f046c2882
Roudi <sajjad.roodi@gmail.com>
2022-06-28 19:08:41 +0430

    Implement running directory server

--- src/main/kotlin/ir/roudi/Application.kt
+++ src/main/kotlin/ir/roudi/Application.kt
@@ -2,11 +2,20 @@ package ir.roudi
 
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import ir.roudi.directory.configureDirectoryRouting
 import ir.roudi.notification.configureNotificationRouting
 import ir.roudi.plugins.*
 
 fun main() {
+    runDirectorySever(false)
     runNotificationServer()
+}
+
+private fun runDirectorySever(wait: Boolean = true) {
+    embeddedServer(Netty, host = Config.DIRECTORY_HOST, port = Config.DIRECTORY_PORT) {
+        configureDirectoryRouting()
+        configureSerialization()
+    }.start(wait = wait)
 }
 
 private fun runNotificationServer(wait: Boolean = true) {
